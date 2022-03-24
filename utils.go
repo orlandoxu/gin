e66bd1e4cfbf0613bdbf73336df727c782ac9b3b
orlandoxu <scuxsl@gmail.com>
2022-03-24 21:49:34 +0800

    删除不要了的

--- utils.go
+++ utils.go
@@ -5,8 +5,6 @@
 package gin
 
 import (
-	"encoding/xml"
-	"net/http"
 	"os"
 	"path"
 	"reflect"
@@ -35,44 +33,8 @@ func Bind(val interface{}) HandlerFunc {
 	}
 }
 
-// WrapF is a helper function for wrapping http.HandlerFunc and returns a Gin middleware.
-func WrapF(f http.HandlerFunc) HandlerFunc {
-	return func(c *Context) {
-		f(c.Writer, c.Request)
-	}
-}
-
-// WrapH is a helper function for wrapping http.Handler and returns a Gin middleware.
-func WrapH(h http.Handler) HandlerFunc {
-	return func(c *Context) {
-		h.ServeHTTP(c.Writer, c.Request)
-	}
-}
-
 // H is a shortcut for map[string]interface{}
 type H map[string]interface{}
-
-// MarshalXML allows type H to be used with xml.Marshal.
-func (h H) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	start.Name = xml.Name{
-		Space: "",
-		Local: "map",
-	}
-	if err := e.EncodeToken(start); err != nil {
-		return err
-	}
-	for key, value := range h {
-		elem := xml.StartElement{
-			Name: xml.Name{Space: "", Local: key},
-			Attr: []xml.Attr{},
-		}
-		if err := e.EncodeElement(value, elem); err != nil {
-			return err
-		}
-	}
-
-	return e.EncodeToken(xml.EndElement{Name: start.Name})
-}
 
 func assert1(guard bool, text string) {
 	if !guard {
